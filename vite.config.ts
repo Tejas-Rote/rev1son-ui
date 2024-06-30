d3e2383e81dd45c811ecb893904b9fea3307cf48
Tejas-Rote <tejas292002@gmail.com>
2024-07-01 01:21:51 +0530

    add:post css config

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import postcss from "rollup-plugin-postcss";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    postcss({
+      modules: {
+        generateScopedName: "[name]__[local]___[hash:base64:5]",
+      },
+      extract: true,
+      minimize: true,
+      sourceMap: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve("src", "./index.ts"),
